f0c85f3fd675ae8ad8ed1b58ea95cab05065f90b
hyoseongLee <hyoseong123@naver.com>
2024-10-25 20:28:36 +0900

    2024.10.25

--- solo/src/App.js
+++ solo/src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import TodoLayout from './component/TodoLayout';
 import TodoList from './component/TodoList';
-import { MdAddCircle } from 'react-icons/md';
-import './App.css';
 import TodoInsert from './component/TodoInsert';
+import './App.css';
+import { MdAddCircle } from 'react-icons/md';
 
 let nextid = 4;
-const App = () => {
-  const [selectedTodo, setSelectedTodo] = useState(null);
-  const [insertToggle, setInsertToggle] = useState(false);
 
+const App = () => {
+
+  const [selectedTodo,setSelectedTodo] = useState (null)
+  const [insertToggle, setInsertToggle] = useState(false)
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -25,73 +26,76 @@ const App = () => {
       id: 3,
       text: "할일 3",
       checked: true
-    },
-  ])
-
-  const onInsertToggle = () => {
-    setInsertToggle(prev => !prev)
-  }
+    }
+  ]);
 
   const onInsertTodo = text => {
     if (text === "") {
-      return alert("할 일을 입력해 주세요.");
-    } else {
+      return alert ("할일을 입력해주세요.")
+    }else {
       const todo = {
         id: nextid,
         text,
-        checked: false
+        checked:false
       };
-      setTodos(todos => todos.concat(todo));
-      nextid++;
-    }
+      setTodos (todos => todos.concat(todo))
+      nextid++
+    } 
   };
 
   const onChangeSelectedTodo = todo => {
-    setSelectedTodo(todo)
-
-  }
-
-  const onCheckToggle = id => {
-    setTodos(todos => todos.map(todo => (
-      todo.id === id ?
-        { ...todo, checked: !todo.checked } : todo
-    )))
-  }
-
-
-  const onRemove = id => {
-    onInsertToggle();
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    setSelectedTodo(todo);
   };
 
-  const onUpdate = (id, text) => {
-    onInsertToggle();
-    setTodos(todos =>
-      todos.map(todo => (todo.id === id ? { ...todo, text } : todo))
-    );
+  const onInsertToggle = () => {
+    if (selectedTodo) {
+      setSelectedTodo(null);
+    }
+    setInsertToggle(prev => !prev);
   };
+
+  const onCheckedToggle = id => {
+    setTodos(todos=> 
+      todos.map(todo => 
+        todo.id === id ? {...todo, checked: !todo.checked} : todo))
+  };
+  
+const onRemove = id => {
+  onInsertToggle();
+  setTodos(todos => todos.filter(todo => todo.id !==id));
+};
+
+const onUpdata = (id,text) => {
+  onInsertToggle();
+  setTodos(todos => todos.map(todo => todo.id ? {...todo, text} : todo))
+}
 
   return (
-    <TodoLayout todoLength={todos.length}>
-      <TodoList
-        todos={todos}
-        onCheckToggle={onCheckToggle}
-        onInsertToggle={onInsertToggle}
-        onChangeSelectedTodo={onChangeSelectedTodo}
-      />
 
-      <div className='add-plus-button' onClick={onInsertToggle}> <MdAddCircle /> </div>
-      {insertToggle &&
-        <TodoInsert
-          selectedTodo={selectedTodo}
-          onInsertToggle={onInsertToggle}
-          onInsertTodo={onInsertTodo}
-          onRemove={onRemove}
-          onUpdate={onUpdate}
-        />}
+    <TodoLayout todolangth={todos.length}>
+      <TodoList 
+      todos={todos} 
+      onInsertTodo={onInsertTodo} 
+      onCheckedToggle={onCheckedToggle} 
+      onChangeSelectedTodo={onChangeSelectedTodo}
+      onInsertToggle={onInsertToggle}
+      />
+      <div className='app-plus-button' onClick={onInsertToggle}>
+        <MdAddCircle />
+        </div>
+      {insertToggle && (
+        <TodoInsert 
+        selectedTodo={selectedTodo}
+        onInsertToggle={onInsertToggle}
+        onInsertTodo={onInsertTodo}
+        onRemove={onRemove}
+        onUpdata={onUpdata}
+        />
+      )}
     </TodoLayout>
   )
-}
+};
+
 
 
 
--- solo/src/component/TodoItem.js
+++ solo/src/component/TodoItem.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
-import './TodoItem.css'
+import './TodoItem.css';
 
+const TodoItem = ({ 
+    todo, 
+    onInsertToggle, 
+    onCheckedToggle, 
+    onChangeSelectedTodo,
+    
 
-const TodoItem = ({ todo, onCheckToggle, onInsertToggle, onChangeSelectedTodo }) => {
+ }) => {
+
     const { id, text, checked } = todo;
-
 
     return (
         <div className='TodoItem'>
-            <div className={`content ${checked ? 'checked' : ''}`}>
-                {checked ? (<MdCheckBox
-                    onClick={() => {
-                        onCheckToggle(id);
-                    }}
-                />
-                ) : (
-                        <MdCheckBoxOutlineBlank
-                            onClick={() => {
-                                onCheckToggle(id);
-                            }} 
-                            />
-                    )}
+            <div className={`content ${checked ? 'checked' : ""}`}>
+                {checked ? (<MdCheckBox onClick={() => { onCheckedToggle(id) }} />) : (
+                    <MdCheckBoxOutlineBlank onClick={() => { onCheckedToggle(id) }} />)}
             </div>
-            <div className='text' onClick={() => {
+
+            <div className="text" onClick={() => {
                 onChangeSelectedTodo(todo);
                 onInsertToggle();
             }}
             >
-                {text} </div>
-        </div>
+                {text}
+            </div>
+        </div >
     )
-}
-
-
-
+};
 
 export default TodoItem;
