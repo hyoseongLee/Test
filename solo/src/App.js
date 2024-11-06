464c1907a47d2a5e7151eac5971231485da4007c
hyoseongLee <hyoseong123@naver.com>
2024-11-06 20:05:48 +0900

    final

--- solo/src/App.js
+++ solo/src/App.js
@@ -1,10 +1,10 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Box from './component/Box';
 import "./App.css";
 
 const choice = {
   rock: {
-    name: "Rock",
+    name: "rock",
     img: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Dwayne_%22The_Rock%22_Johnson_Visits_the_Pentagon_%2841%29_%28cropped%29.jpg'
   },
   scissor: {
@@ -18,31 +18,47 @@ const choice = {
 };
 
 function App() {
-  const [userSelect,setUserSelect] = useState(null);
-  const [computerSelect,setComputerSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+
   const play = (userChoice) => {
     setUserSelect(choice[userChoice])
     let computerChoice = randomChoice()
     setComputerSelect(computerChoice)
+    setResult(judgment(choice[userChoice], computerChoice))
   };
 
-  const randomChoice=()=> {
+  const judgment = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    if (user.name === computer.name) { return "tie" }
+    else if (user.name == 'rock')
+      return computer.name == "Scissor" ? "win" : "lose"
+    else if (user.name == 'scissor')
+      return computer.name == "paper" ? "win" : "lose"
+    else if (user.name == 'paper')
+      return computer.name == "Rock" ? "win" : "lose"
+  };
+
+  const randomChoice = () => {
     let itemArray = Object.keys(choice); /** 객체에 키 값만 뽑아서 어레이로 만들어주는 함수다. */
-    let randomItem = Math.floor(Math.random()*itemArray.length); /**Math.floor는 소수 앞자리 제외 버리는 함수. */
+    let randomItem = Math.floor(Math.random() * itemArray.length); /**Math.floor는 소수 앞자리 제외 버리는 함수. */
     let final = itemArray[randomItem]
     return choice[final];
   };
 
   return (
     <div>
+      <div className='title'> Rock Scissor Paper Game </div>
       <div className='main'>
-        <Box title="You" item={userSelect}/>
-        <Box title="computer" item={computerSelect} />
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="computer" item={computerSelect} result={result} />
       </div>
       <div className='main'>
-        <button onClick={() => play("scissor")}> scissor </button>
-        <button onClick={() => play("rock")}> rock</button>
-        <button onClick={() => play("paper")} >paper</button>
+        <button onClick={() => play("scissor")}> Scissor </button>
+        <button onClick={() => play("rock")}> Rock </button>
+        <button onClick={() => play("paper")} >Paper </button>
       </div>
     </div>
   )
--- solo/src/component/Box.js
+++ solo/src/component/Box.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
-
 const Box = (props) => {
+  let resultMessage;
+  // 컴퓨터가 승/패/비김에 따른 결과 처리
+  if (props.result === "win") {
+    resultMessage = props.title === "You" ? "You Win!" : "Computer Lose!";
+  } else if (props.result === "lose") {
+    resultMessage = props.title === "You" ? "You Lose!" : "Computer Win!";
+  } else if (props.result === "tie") {
+    resultMessage = "It's a Tie!";
+  } else {
+    resultMessage = "";
+  }
+
   return (
-    <div className='box'>
-      <h1> {props.title} </h1>
-      <img className='item-img' src={props.item && props.item.img} />
-      <h2> computer</h2>
+    <div className={`box ${props.result}`}>
+      <h1>{props.title}</h1>
+      <h2 data-testid="item-name">{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} alt={props.item?.name} />
+      <h2>{resultMessage}</h2>
     </div>
-  )
-}
+  );
+};
 
 export default Box;
